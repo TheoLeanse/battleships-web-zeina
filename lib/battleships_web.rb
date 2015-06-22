633b1c1b11f80a9b4e772bf2642cd98814ad0ff7
Zeina Dajani <dajani.zeina@gmail.com>
2015-06-22 17:01:29 +0100

    new game link problem

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,10 +1,14 @@
 require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
+  set :views, proc { File.join(root, '..', 'views') }
   get '/' do
-    'Hello BattleshipsWeb!'
+    erb :index
   end
 
+  # get '/new_game' do
+  #
+  # end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
